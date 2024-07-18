04783dd6610b0d6995dfebffeb083126d8ca1959
Taifinkaba <taifinkabad@gmail.com>
2024-07-18 12:12:40 -0400

    2 parts done  hw helps a lot

--- src/0-has-many-belongs-to.js
+++ src/0-has-many-belongs-to.js
@@ -1,15 +1,62 @@
 const { getId } = require('./utils');
 
 class ToDoItem {
-  constructor() {
+  constructor(description, priorityLevel) {
     this.id = getId();
+    this.priorityLevel=priorityLevel;
+    this.description=description;
+    this.isDone=false;
+    //extra property
+    this.extra='Hi I\'m Tai!';
+  }
+
+  //extra method
+  extraMethod(){
+    this.extra='Omg I\'m Tai too!!';
   }
 }
 
 
+
 class ToDoList {
-  constructor() {
+  constructor(name, items) {
     this.id = getId();
+    this.name=name;
+    this.items=[];
+
+    // .instances static property that access a instance in all the classes and keeps it private
+    if (!ToDoList.instances) {
+      ToDoList.instances = [];
+    }
+    ToDoList.instances.push(this);
+  }
+
+  createItem(description,priorityLevel){
+    const newItem = new ToDoItem(description, priorityLevel);
+    this.items.push(newItem);
+    return newItem;
+  }
+
+  getItems(){
+    return [...this.items];
+  }
+
+  getCompletedCount(){
+    return this.items.filter(item => item.isDone).length;
+  }
+
+  static list() {
+    return [...ToDoList.instances];
+
+  }
+
+  static findBy(id){
+    return ToDoList.instances.find(list => list.id === id);
+  }
+
+  //extra instance method
+  extraInstanceMethod(){
+    return this.extra='Tai, you wanna get some Thai?';
   }
 }
 
--- src/1-inheritance-polymorphism.js
+++ src/1-inheritance-polymorphism.js
@@ -1,9 +1,51 @@
 class Phone {
+  constructor(phoneNumber,contacts){
+    this.phoneNumber=phoneNumber;
+    this.contacts=[]
+  }
 
+  addContact(contact){
+    // .test regex method returns true or false to check if the phone number is 10 digits
+    if (!contact.name || !contact.phoneNumber || typeof contact.phoneNumber !== 'string' || !(/^\d{10}$/).test(contact.phoneNumber)) {
+      return 'Invalid';
+    }
+    this.contacts.push(contact);
+    return `${contact.name} added.`;
+  }
+
+  removeContact(name){
+    const index = this.contacts.findIndex(contact => contact.name === name);
+    if (index === -1) {
+      return 'Contact not found.';
+    }
+    this.contacts.splice(index, 1);
+    return `${name} removed.`;
+  }
+
+  makeCall(nameOrNumber){
+    const contact = this.contacts.find(contact => contact.name === nameOrNumber || contact.phoneNumber === nameOrNumber);
+    if (contact) {
+      return `Calling ${contact.name}...`;
+    } else if (/^\d{10}$/.test(nameOrNumber)) {
+      return `Calling ${nameOrNumber}...`;
+    } else {
+      return 'Invalid';
+    }
+  }
 }
 
-class AppleIPhone {
+class AppleIPhone extends Phone {
+  constructor(phoneNumber, model){
+    super(phoneNumber);
+    this.model=model;
+  }
 
+  sendIMessage(phone, message){
+    if (phone.constructor !== AppleIPhone) {
+      return 'Message could not be sent.';
+    }
+    return `Message: ${message} - sent from ${this.model}`;
+  }
 }
 
 module.exports = {
